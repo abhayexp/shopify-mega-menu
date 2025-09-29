c40021b116ccb56edf533daa265062ed710edc2e
abhayexp <abhay.k@experionglobal.com>
2025-09-29 18:58:33 +0530

    Fixed admin Menu Add and  Edit Featuren Bugs

--- exp-mega-menu/app/components/MegaMenuFull.tsx
+++ exp-mega-menu/app/components/MegaMenuFull.tsx
@@ -1,175 +1,188 @@
-import React, { useState, useEffect } from 'react';
-import {
-  Frame,
-  Button,
-  TextField,
-  Card,
-  Thumbnail,
-  BlockStack,
-  DropZone,
-  Text
-} from '@shopify/polaris';
-import TreeEditor from './TreeEditor';
-import TreePreview from './TreePreview';
-import TreeModal from "../components/TreeModal";
-import TreeView from './TreeView';
-import { v4 as uuidv4 } from 'uuid';
-import MenuGrid from './MenuGrid';
-import { accessSync } from 'fs';
-import { useCallback } from 'react';
-import AdditionalHelp from './AdditionalHelp';
-import { json } from 'stream/consumers';
-import { Images } from 'lucide-react';
+  import React, { useState, useEffect } from 'react';
+  import {
+    Frame,
+    Button,
+    TextField,
+    Card,
+    Thumbnail,
+    BlockStack,
+    DropZone,
+    Text
+  } from '@shopify/polaris';
+  import TreeEditor from './TreeEditor';
+  import TreePreview from './TreePreview';
+  import TreeModal from "../components/TreeModal";
+  import TreeView from './TreeView';
+  import { v4 as uuidv4 } from 'uuid';
+  import MenuGrid from './MenuGrid';
+  import { accessSync } from 'fs';
+  import { useCallback } from 'react';
+  import AdditionalHelp from './AdditionalHelp';
+  import { json } from 'stream/consumers';
+  import { Images } from 'lucide-react';
 
- export interface TreeNode {
-  id: string;
-  label: string;
-  title: string;
-  linkType: 'custom' | 'collection' | 'product' | 'page';
-  linkValue: string;
-  children: TreeNode[];
-}
+  export interface TreeNode {
+    id: string;
+    label: string;
+    title: string;
+    linkType: 'custom' | 'collection' | 'product' | 'page';
+    linkValue: string;
+    children: TreeNode[];
+  }
 
-interface MegaMenu {
-  id: string,
-  title: string,
-  items: TreeNode[],
-}
+  interface MegaMenu {
+    id: string,
+    title: string,
+    items: TreeNode[],
+  }
 
-export default function MegaMenuFullScreenLayout() {
+  export default function MegaMenuFullScreenLayout() {
 
-  const [active, setActive] = useState(false);
-  const [tree, setTree] = useState<TreeNode[]>([]);
-  const [menu, setMenu] = useState<MegaMenu>({
+    const [active, setActive] = useState(false);
+    const [tree, setTree] = useState<TreeNode[]>([]);
+    const [menu, setMenu] = useState<MegaMenu>({
+      id: `menu-${uuidv4()}`,
+      title: '',
+      items: [],
+    });
+
+
+    const [ menuFlag, setMenuFlag ] = useState(false);
+    const [customize, setCustomise] = useState<MegaMenu>();
+    const [isCustomise, setIsCustomise] = useState<boolean>(false);
+
+    const [isRootModalOpen, setIsRootModalOpen] = useState(false);
+    const handleOpenRootModal = () => setIsRootModalOpen(true);
+    const handleCloseRootModal = () => setIsRootModalOpen(false);
+
+
+
+
+
+
+    const toggleActive = () =>{
+    console.log("toggle active called", tree);
+    setTree([]);
+    setActive(prev => !prev);
+  }
+
+  const handleAddMenu = () => {
+  setIsCustomise(false);
+  setCustomise(undefined);
+  setTree([]); // fresh tree
+  setMenu({
     id: `menu-${uuidv4()}`,
     title: '',
     items: [],
   });
+  setActive(prev => !prev);
+};
 
 
+    useEffect(() => {
+      document.body.style.overflow = active ? 'hidden' : 'auto';
+      return () => (document.body.style.overflow = 'auto');
+    }, [active]);
 
-  const [customize, setCustomise] = useState<MegaMenu>();
-  const [isCustomise, setIsCustomise] = useState<boolean>(false);
+    //  Add  MegaMenu 
+    const handleMenuSubmit = async () => {
+      const finalMenu = {
+        ...menu,
+        items: tree,
+      };
 
-  const [isRootModalOpen, setIsRootModalOpen] = useState(false);
-  const handleOpenRootModal = () => setIsRootModalOpen(true);
-  const handleCloseRootModal = () => setIsRootModalOpen(false);
+      console.log("final menu ",finalMenu);
 
-
-
-
-
-
-  const toggleActive = () =>{
-    setTree([]);
-   setActive(prev => !prev);
-  }
-    
-
-  useEffect(() => {
-    document.body.style.overflow = active ? 'hidden' : 'auto';
-    return () => (document.body.style.overflow = 'auto');
-  }, [active]);
-
-  //  Add  MegaMenu 
-  const handleMenuSubmit = async () => {
-    const finalMenu = {
-      ...menu,
-      items: tree,
-    };
-
-    console.log("final menu ",finalMenu);
-
-    // call menu upload api
-    fetch("http://localhost:5000/api/add-menu", {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(finalMenu)
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("API Response:", data);
-        setMenu({
-          id: `menu-${uuidv4()}`,
-          title: '',
-          items: []
-        });
-        setTimeout(() => {
-          setActive(prev => !prev);
-        }, 3000);
+      // call menu upload api
+      fetch("http://localhost:5000/api/add-menu", {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(finalMenu)
       })
-      .catch((error) => {
-        console.log(" Error In API ", error);
-      })
-  }
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("API Response:", data);
+          setMenu({
+            id: `menu-${uuidv4()}`,
+            title: '',
+            items: []
+          });
+          setTimeout(() => {
+            setActive(prev => !prev);
+          }, 3000);
+        })
+        .catch((error) => {
+          console.log(" Error In API ", error);
+        })
+    }
 
-  // handle customisation
-  const handleCustomisation = (obj: MegaMenu, id: string) => {
-    console.log("mega menu gen", obj);
-    console.log(` Customisation clicked for ${id}`, obj);
-    setIsCustomise(true);
-    setCustomise(obj);
-    toggleActive();
-    setMenu(prev => ({
-      ...prev,
-      title: obj.title,
-      id: `menu-${uuidv4()}`,
-      items: obj.items
-    }));
-    setTree(obj.items); 
+    // handle customisation
+    const handleCustomisation = (obj: MegaMenu, id: string) => {
+      console.log("mega menu gen", obj);
+      console.log(` Customisation clicked for ${id}`, obj);
+      setIsCustomise(true);
+      setCustomise(obj);
+      toggleActive();
+      setMenu(prev => ({
+        ...prev,
+        title: obj.title,
+        id: `menu-${uuidv4()}`,
+        items: obj.items
+      }));
+      setTree(obj.items); 
 
-  }
+    }
 
-  console.log("customise data", customize);
+    console.log("customise data", customize);
 
-  return (
-    <Frame>
-      <div style={{ marginTop: "10px" }}>
-        <Button onClick={toggleActive}>Add Mega Menu</Button>
-      </div>
+    return (
+      <Frame>
+        <div style={{ marginTop: "10px" }}>
+          <Button onClick={handleAddMenu}>Add Mega Menu</Button>
+        </div>
 
-      {active && (
-        <div style={{ position: 'fixed', inset: 0, background: '#fff', zIndex: 9999, display: 'flex', flexDirection: 'column' }}>
-          {/* Header */}
-          <div style={{ padding: 16, borderBottom: '1px solid #ccc', display: 'flex', justifyContent: 'space-between', backgroundColor: '#f6f6f7' }}>
-            <strong>Mega Menu #{Math.floor(Math.random() * 100000)}</strong>
-            <div style={{ display: 'flex', gap: 8 }}>
-              <Button onClick={toggleActive} >Cancel</Button>
-              <Button size="medium" variant="primary" onClick={handleMenuSubmit}>Save</Button>
+        {active && (
+          <div style={{ position: 'fixed', inset: 0, background: '#fff', zIndex: 9999, display: 'flex', flexDirection: 'column' }}>
+            {/* Header */}
+            <div style={{ padding: 16, borderBottom: '1px solid #ccc', display: 'flex', justifyContent: 'space-between', backgroundColor: '#f6f6f7' }}>
+              <strong>Mega Menu #{Math.floor(Math.random() * 100000)}</strong>
+              <div style={{ display: 'flex', gap: 8 }}>
+                <Button onClick={toggleActive} >Cancel</Button>
+                <Button size="medium" variant="primary" onClick={handleMenuSubmit}>Save</Button>
+              </div>
+            </div>
+
+            {/* Body */}
+            <div style={{ display: 'flex' }}>
+              {/* Left */}
+              <div style={{ width: '60%', borderRight: '1px solid #ccc', padding: 20, overflowY: 'auto' }}>
+                <TextField
+                  label="Menu Name"
+                  value={menu.title}
+                  onChange={(value) => setMenu(prev => ({ ...prev, title: value }))}
+                  autoComplete="off"
+                />
+                <TreeEditor tree={tree} setTree={setTree} isCostomize={isCustomise} data={customize} />
+              </div>
+
+
+              {/* Right */}
+              <div style={{ width: '45%', padding: 24, overflowY: 'auto' }}>
+                <TreeView tree={tree} setTree={setTree} isCostomize={isCustomise} data={customize}  />
+              </div>
             </div>
           </div>
+        )}
 
-          {/* Body */}
-          <div style={{ display: 'flex' }}>
-            {/* Left */}
-            <div style={{ width: '60%', borderRight: '1px solid #ccc', padding: 20, overflowY: 'auto' }}>
-              <TextField
-                label="Menu Name"
-                value={menu.title}
-                onChange={(value) => setMenu(prev => ({ ...prev, title: value }))}
-                autoComplete="off"
-              />
-              <TreeEditor tree={tree} setTree={setTree} isCostomize={isCustomise} data={customize} />
-            </div>
-
-
-            {/* Right */}
-            <div style={{ width: '45%', padding: 24, overflowY: 'auto' }}>
-              <TreeView tree={tree} setTree={setTree} isCostomize={isCustomise} data={customize}  />
-            </div>
+        {/*  display Grid below */}
+        {!active &&
+          <div style={{ marginTop: "20px" }}>
+            <MenuGrid onCustomise={handleCustomisation}></MenuGrid>
           </div>
-        </div>
-      )}
-
-      {/*  display Grid below */}
-      {!active &&
-        <div style={{ marginTop: "20px" }}>
-          <MenuGrid onCustomise={handleCustomisation}></MenuGrid>
-        </div>
-      }
-      {/* <div>
-        <AdditionalHelp></AdditionalHelp>
-      </div> */}
-    </Frame>
-  );
-}
+        }
+        {/* <div>
+          <AdditionalHelp></AdditionalHelp>
+        </div> */}
+      </Frame>
+    );
+  }
--- exp-mega-menu/app/components/TreeView.tsx
+++ exp-mega-menu/app/components/TreeView.tsx
@@ -25,13 +25,32 @@ const defaultLinkTypeOptions: SelectOption[] = [
   { label: "Page", value: "page" },
 ];
 
-const TreeView: React.FC<TreeEditorProps> = ({ tree, setTree, isCostomize, data }) => {
+const TreeView: React.FC<TreeEditorProps> = ({
+  tree,
+  setTree,
+  isCostomize,
+  data,
+}) => {
   const [label, setLabel] = useState("");
   const [linkType, setLinkType] = useState("custom");
   const [parentId, setParentId] = useState<string | null>(null);
   const [editingNode, setEditingNode] = useState<TreeNode | null>(null);
-  const [linkTypeOptions, setLinkTypeOptions] = useState<SelectOption[]>(defaultLinkTypeOptions);
+  const [linkTypeOptions, setLinkTypeOptions] = useState<SelectOption[]>(
+    defaultLinkTypeOptions,
+  );
 
+
+  useEffect(() => {
+    if (isCostomize && data?.items) {
+      setTree(data.items);
+    }
+    else {
+      setTree([]);
+    }
+  }, [isCostomize, data]);
+
+
+  
   // Add Node
   const handleAddNode = () => {
     if (!label.trim()) return;
@@ -65,50 +84,83 @@ const TreeView: React.FC<TreeEditorProps> = ({ tree, setTree, isCostomize, data 
   };
 
   // Update Node
-  const updateTreeNode = (nodes: TreeNode[], updatedNode: TreeNode): TreeNode[] =>
-    nodes.map(node =>
+  const updateTreeNode = (
+    nodes: TreeNode[],
+    updatedNode: TreeNode,
+  ): TreeNode[] =>
+    nodes.map((node) =>
       node.id === updatedNode.id
         ? { ...node, ...updatedNode }
-        : { ...node, children: updateTreeNode(node.children, updatedNode) }
+        : { ...node, children: updateTreeNode(node.children, updatedNode) },
     );
 
   // Delete Node
-    const deleteFromTree = (nodes: TreeNode[], nodeId: string): TreeNode[] =>{
-      console.log(" nodes", nodes);
-      console.log("Node ID ", nodeId);
+  const deleteFromTree = (nodes: TreeNode[], nodeId: string): TreeNode[] => {
+    console.log(" nodes", nodes);
+    console.log("Node ID ", nodeId);
     const newNodes = nodes
-        .filter(node => node.id !== nodeId)
-        .map(node => ({ ...node, children: deleteFromTree(node.children, nodeId) }));
-      console.log(" finale node ", newNodes);
-      return  newNodes
-    }
-    
-const handleDeleteNode = (nodeId: string) => {
-  setTree(prevTree => {
-    const newTree = deleteFromTree(prevTree, nodeId);
-    return newTree;
-  });
+      .filter((node) => node.id !== nodeId)
+      .map((node) => ({
+        ...node,
+        children: deleteFromTree(node.children, nodeId),
+      }));
+    console.log(" finale node ", newNodes);
+    return newNodes;
+  };
 
-  if (editingNode?.id === nodeId) setEditingNode(null);
-};
+  const handleEditNode = (node: TreeNode) => {
+    console.log("Editing node: ", node);
+    // setEditingNode(node);
+  };
 
- 
+  const handleDeleteNode = (nodeId: string) => {
+    setTree((prevTree) => {
+      const newTree = deleteFromTree(prevTree, nodeId);
+      return newTree;
+    });
 
-
-
-
+    if (editingNode?.id === nodeId) setEditingNode(null);
+  };
 
   // Render Tree recursively
   const renderTree = (nodes: TreeNode[]) =>
-    nodes.map(node => (
-      <div key={node.id} style={{ marginLeft: 20, marginTop: 10, borderLeft: "1px dashed #ccc", paddingLeft: 10 }}>
-        <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+    nodes.map((node) => (
+      <div
+        key={node.id}
+        style={{
+          marginLeft: 20,
+          marginTop: 10,
+          borderLeft: "1px dashed #ccc",
+          paddingLeft: 10,
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
           <div>
             <strong>{node.label}</strong> – <em>{node.linkType}</em>
           </div>
           <div style={{ display: "flex", gap: 6 }}>
-            <Button icon={<Icon source={PlusMinor} />} onClick={() => setParentId(node.id)} />
-            <Button size="slim" onClick={() => setEditingNode(node)}>Edit</Button>
+            <Button
+              icon={<Icon source={PlusMinor} />}
+              onClick={() => setParentId(node.id)}
+            />
+            <Button
+              size="slim"
+              onClick={() =>
+                setEditingNode({
+                  ...node,
+                  label: node.label ?? "",
+                  linkType: node.linkType ?? "custom",
+                })
+              }
+            >
+              Edit
+            </Button>
             <Button
               size="slim"
               tone="critical"
@@ -117,13 +169,83 @@ const handleDeleteNode = (nodeId: string) => {
               Delete
             </Button>
           </div>
+
+          {/*  If user click on  edit  edit modal show up */}
+          {editingNode?.id == node?.id && (
+            <div>
+              <div
+                style={{
+                  marginTop: 8,
+                  padding: 8,
+                  display: "block",
+                  width: "200px",
+                  // alignItems: "flex-end",
+                  gap: 12,
+                }}
+              >
+                <div>
+                  <TextField
+                    label="Category name"
+                    value={editingNode?.label}
+                    onChange={(val) => 
+                        setEditingNode({ ...editingNode, label: val })
+                    }
+                    autoComplete="off"  
+                  />
+                  <Select
+                    label="Link"
+                    options={linkTypeOptions}
+                    value={editingNode.linkType ?? "custom"}
+                    onChange={(val) =>
+                      setEditingNode((prev: any) =>
+                        prev ? { ...prev, linkType: val } : prev,
+                      )
+                    }
+                  />
+                </div>
+                {/* {editingNode.images && (
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      marginTop: "10px",
+                      gap: "10px",
+                    }}
+                  >
+                    <p>Show Slider</p>
+                    <Button onClick={() => handleToggle(editingNode, data.id)}>
+                      {active ? "Turn On" : "Turn Off"}
+                    </Button> 
+                  </div>
+                )} */}
+                <Button
+                  onClick={() => {
+                    setTree((prev) => updateTreeNode(prev, editingNode)); // update the tree recursively
+                    setEditingNode(null); // close editing section
+                  }}
+                >
+                  Update
+                </Button>
+              </div>
+            </div>
+          )}
         </div>
 
         {/* Child Add Form */}
         {parentId === node.id && (
           <div style={{ marginTop: 8, padding: 8, display: "flex", gap: 12 }}>
-            <TextField label="Child Name" value={label} onChange={setLabel} autoComplete="off" />
-            <Select label="Link Type" options={linkTypeOptions} value={linkType} onChange={setLinkType} />
+            <TextField
+              label="Child Name"
+              value={label}
+              onChange={setLabel}
+              autoComplete="off"
+            />
+            <Select
+              label="Link Type"
+              options={linkTypeOptions}
+              value={linkType}
+              onChange={setLinkType}
+            />
             <Button onClick={handleAddNode}>Add</Button>
           </div>
         )}
@@ -139,7 +261,10 @@ const handleDeleteNode = (nodeId: string) => {
         const res = await fetch("http://localhost:5000/api/collection");
         if (!res.ok) throw new Error(`Status: ${res.status}`);
         const result = await res.json();
-        const options: SelectOption[] = result.collections.map((c: any) => ({ label: c.title, value: c.handle }));
+        const options: SelectOption[] = result.collections.map((c: any) => ({
+          label: c.title,
+          value: c.handle,
+        }));
         setLinkTypeOptions([...defaultLinkTypeOptions, ...options]);
       } catch (err) {
         console.log(err);
@@ -150,12 +275,21 @@ const handleDeleteNode = (nodeId: string) => {
 
   return (
     <div style={{ marginTop: 20 }}>
-      <div style={{ maxHeight: 500, overflowY: "auto", border: "1px solid #ccc", padding: 10, borderRadius: 6 }}>
-        {isCostomize && data ?  renderTree(tree) :renderTree(data.items)  }
+      <div
+        style={{
+          maxHeight: 500,
+          overflowY: "auto",
+          border: "1px solid #ccc",
+          padding: 10,
+          borderRadius: 6,
+        }}
+      >
+        {/* {isCostomize && data ? renderTree(tree) : renderTree([])} */}
+        {renderTree(tree)}
       </div>
 
       {/* Edit Modal */}
-      {editingNode && (
+      {/* {editingNode && (
         <Modal
           open={true}
           onClose={() => setEditingNode(null)}
@@ -163,11 +297,13 @@ const handleDeleteNode = (nodeId: string) => {
           primaryAction={{
             content: "Save",
             onAction: () => {
-              setTree(prev => updateTreeNode(prev, editingNode));
+              setTree((prev) => updateTreeNode(prev, editingNode));
               setEditingNode(null);
             },
           }}
-          secondaryActions={[{ content: "Cancel", onAction: () => setEditingNode(null) }]}
+          secondaryActions={[
+            { content: "Cancel", onAction: () => setEditingNode(null) },
+          ]}
         >
           <Modal.Section>
             <TextField
@@ -179,11 +315,13 @@ const handleDeleteNode = (nodeId: string) => {
               label="Link Type"
               options={linkTypeOptions}
               value={editingNode.linkType}
-              onChange={(val) => setEditingNode({ ...editingNode, linkType: val })}
+              onChange={(val) =>
+                setEditingNode({ ...editingNode, linkType: val })
+              }
             />
           </Modal.Section>
         </Modal>
-      )}
+      )} */}
     </div>
   );
 };
